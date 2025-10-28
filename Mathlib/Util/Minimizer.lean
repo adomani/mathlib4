cefe21c32492b9bec4c93856b8bba19b1e8ae2c1
adomani <adomani@gmail.com>
2025-10-28 22:27:17 +0000

    deal with module index and current file

--- Mathlib/Util/Minimizer.lean
+++ Mathlib/Util/Minimizer.lean
@@ -4,6 +4,7 @@
 import Mathlib.Tactic.MinImports
 import Aesop
 import Mathlib.adomaniLeanUtils.inspect_syntax
+import Mathlib.Tactic
 
 open Lean Elab Command
 elab "repl " cmd:command : command => do
@@ -27,8 +28,17 @@ def printThm (cinfo : ConstantInfo) (bySorry? : Bool) : Meta.MetaM MessageData :
   --let some f := (← getEnv).find? declName | throwError "The constant {declName} should exist..."
   let isProp ← Meta.isProp cinfo.type
   let cmdTxt := if isProp then "theorem" else "def"
-  if cinfo.value?.isNone then logInfo m!"{.ofConstName declName} has no value!"
-  let proof := if isProp && bySorry? then m!"by sorry" else m!"{cinfo.value! (allowOpaque := true)}"
+  --if cinfo.value?.isNone then
+  --  if (cinfo.value? (allowOpaque := true)).isNone then
+  --    logInfo m!"{.ofConstName declName} has not even an opaque value!"
+  --  else
+  --    logInfo m!"{.ofConstName declName} has no value!"
+  let proof :=
+    if isProp && bySorry?
+    then
+      m!"by sorry"
+    else
+      ((cinfo.value? (allowOpaque := true)).map (m!"{·}")).getD m!"by sorry"
   return m!"{cmdTxt} {← addMessageContext <| .signature declName} := {proof}"
   --let some proof :=
   --  cmd.raw.find? (#[``Parser.Command.declValEqns, ``Parser.Command.declValSimple].contains ·.getKind) |
@@ -43,6 +53,7 @@ elab "cst " cmd:command : command => do
   --  cmd.raw.find? (·.isOfKind ``Parser.Command.declId) | throwError "No declaration id!"
   let declName ← resolveGlobalConstNoOverload id
   let csts ← getAllDependencies cmd id
+  --dbg_trace csts.toArray
   let cstsAndMods : NameMap (Array ConstantInfo)  ← csts.foldlM (init := ∅) fun tot decl => do
     if decl.isInternalDetail then return tot
     let some cinfo := env.find? decl | return tot
@@ -54,23 +65,34 @@ elab "cst " cmd:command : command => do
   let dRanges := declRangeExt.getModuleEntries env
   --dbg_trace (dRanges.find? `Mathlib.Command.MinImports.getDeclName).map (·.range.pos)
   --if false then
+  liftTermElabM do
   let mut msg := #[]
   for (mod, cs) in sorted do
-    let modIdx := (env.moduleIdxForModule? mod).getD default
-    let dict := dRanges modIdx
+    --let modIdx := (env.moduleIdxForModule? mod).getD (default)
+    --if modIdx == default then
+    --  dbg_trace "default module index for {mod}!"
+    let dict :=
+      if let some modIdx := env.moduleIdxForModule? mod
+      then
+        dRanges modIdx
+      else
+        (declRangeExt.getState env).toArray
     msg := msg.push m!"/- From {mod} -/"
     let cs := cs.qsort fun d1 d2 : ConstantInfo =>
       match dict.find? (·.1 == d1.name), dict.find? (·.1 == d2.name) with
       | some (_, r1), some (_, r2) =>
         r1.range.pos.line < r2.range.pos.line
+      | none, none => dbg_trace "false {(d1.name, d2.name)}"; false
       | _, _ => dbg_trace "false"; false
+      --| some _, _ => dbg_trace "false {d2.name}"; false
+      --| _, some _ => dbg_trace "false {d1.name}"; false
     for cinfo in cs do
       --if c.isInternalDetail then continue
       --let some cinfo := env.find? c | continue
       --let mod := (← findModuleOf? c).getD (← getMainModule)
       --if mod.getRoot == `Mathlib then
       --if c == `X.F then dbg_trace "found in {mod}"
-      msg := msg.push (← liftTermElabM <| printThm cinfo (cinfo.name != declName)) |>.push ""
+      msg := msg.push (← printThm cinfo (cinfo.name != declName)) |>.push ""
       --logInfo <| ← liftTermElabM <| printThm declName true
   logInfo <| m!"\n".joinSep msg.toList
 /-
@@ -85,7 +107,63 @@ elab "cst " cmd:command : command => do
   logInfo <| m!"{id}" ++
               m!"{csts.toArray.qsort (·.toString < ·.toString )}"
 -/
+#print Group
 
+#print axioms funext
+#print sig Ring
+#print equations Nat.add
+run_cmd
+  let stx ← `(command| /-- -/ class Group where mul : Unit)
+  --dbg_trace stx.raw[0]
+  dbg_trace stx.raw[1]
+  dbg_trace ""
+  dbg_trace stx.raw[1][0]
+  dbg_trace ""
+  dbg_trace stx.raw[1][1]
+  dbg_trace ""
+  --elabPrintSig (mkNode `def #[mkIdent `hi, mkIdent `hi, mkIdent `Group])
+  elabPrintEqns (mkNode `def #[mkIdent `hi, mkIdent `hi, mkIdent `Nat.add])
+namespace F
+class Group.{u} (G : Type u) extends
+--number of parameters: 1
+--parents:
+  --Group.toDivInvMonoid :
+  DivInvMonoid G where
+--fields:
+  --mul : G → G → G
+  --mul_assoc : ∀ (a b c : G), a * b * c = a * (b * c)
+  --one : G
+  --one_mul : ∀ (a : G), 1 * a = a
+  --mul_one : ∀ (a : G), a * 1 = a
+  npow :=
+    npowRecAuto
+  npow_zero := by
+    intros; rfl
+  npow_succ := by
+    intros; rfl
+  --inv : G → G
+  div := DivInvMonoid.div'
+  div_eq_mul_inv := by
+    intros; rfl
+  zpow :=
+    zpowRec
+  zpow_zero' := by
+    intros; rfl
+  zpow_succ' := by
+    intros; rfl
+  zpow_neg' := by
+    intros; rfl
+  inv_mul_cancel : ∀ (a : G), a⁻¹ * a = 1
+--constructor:
+--  Group.mk.{u} {G : Type u} [toDivInvMonoid : DivInvMonoid G] (inv_mul_cancel : ∀ (a : G), a⁻¹ * a = 1) : Group G
+--
+#check _root_.Group
+cst
+theorem ga {G} [Group G] {g : G} : g * 1 = g := mul_one g
+
+#exit
+cst
+def mR := Group
 namespace X
 inspect
 theorem E : True := by
