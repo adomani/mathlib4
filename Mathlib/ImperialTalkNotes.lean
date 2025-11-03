6619dfc7f85e9152ea875c42e398b1f89d12c404
adomani <adomani@gmail.com>
2025-11-03 23:09:41 +0000

    writing the talk

--- Mathlib/ImperialTalkNotes.lean
+++ Mathlib/ImperialTalkNotes.lean
@@ -155,14 +155,7 @@ There are a few more `Config`s that are easy to interpret.
 * `introMerge` does what is expected: merge consecutive uses of `intro`.
 -/
 
-variable (nnn : Nat)
-
-
-inspect #check Nat.succ
-inspect #check nnn.succ
 open Lean.Elab
-inspectIT #check Nat.succ
-inspectIT #check nnn.succ
 
 set_option linter.unusedTactic false
 set_option linter.style.multiGoal false
@@ -234,4 +227,162 @@ example : 0 = 0 := by
 #check Lean.Parser.Tactic.open
 #check Lean.Parser.Tactic.match
 #check Lean.Parser.Tactic.unknown
-#exit
+
+namespace Talk
+/-!
+# What happens when you type a command in Lean?
+
+This is a rough and not entirely correct outline.
+
+## Parsing
+First, Lean is going to produce a `Syntax` object from what you typed.
+-/
+#print Syntax
+
+inductive Syntax where
+  | missing
+  | node   (info : SourceInfo) (kind : SyntaxNodeKind) (args : Array Syntax)
+  | atom   (info : SourceInfo) (val : String)
+  | ident  (info : SourceInfo) (rawVal : Substring) (val : Name)
+    (preresolved : List Syntax.Preresolved)
+
+-- `inspect` is a command that is only defined for this presentation!
+--inspect #check Nat
+--inspect section
+
+run_cmd
+  let h := mkIdent `hello
+  let stx ← `(#check $h .succ)
+  let stx ← `(dite cond (fun h => t) (fun h => t))
+  let stx ← `(infixl:25 " ≃* " => MulEquiv)
+  logInfo (InspectSyntax.toMessageData stx)
+
+/-!
+## Elaboration
+Once Lean produced a valid `Syntax` term, it next `elaborates` it.
+
+During elaboration, all sorts of results can happen:
+* a whole bunch of declarations are added to the `Environment` (e.g. you are `import`ing a module);
+* a new declaration could be added to the `Environment` (e.g. you are proving a `theorem`);
+* a `namespace` could be opened;
+* an `attribute` may be added to a previously defined declaration;
+* an option may be set (e.g. `set_option autoImplicit false`);
+* a new `notation` may be introduced (e.g. `infixl:25 " ≃* " => MulEquiv`);
+* a new command may be defined (e.g. you are using `elab`);
+and so on.
+
+Most of these effects, modify the `Environment`.
+At the same time, what the `Environment` is when a command is issued, determines how the command
+is elaborated!
+-/
+#print Environment
+/-!
+The `Environment` is not the only data structure that affects elaboration of commands.
+Modifying the `Scope` also affects elaboration -- the `Scope` is not part of the `Environment`.
+-/
+
+#print Scope
+/-!
+The `Scope` is a `structure` that carries additional information useful for elaborating commands.
+It is part of the state of the `CommandElabM` monad.
+-/
+variable (nnn : Nat)
+
+#where
+
+/-!
+## Quiz
+
+What is the difference in the `Syntax` trees for the following command?
+-/
+
+#check Nat.succ
+#check nnn.succ
+
+/-!
+## Answer
+Let's `inspect` them!
+-/
+
+--inspect #check Nat.succ
+--inspect #check nnn.succ
+
+/-!
+What changes?
+
+`#check` discards the effects of its computations from the `Environment`, so there is no persistent
+data left after a `#check`.
+
+However, elaboration is a complicated process and there is a data-structure that contains basically
+*all* the information about each command: the `InfoTree`s.
+-/
+
+#print InfoTree
+/-!
+The construction of an `InfoTree` for a command is intertwined with the elaboration of the command
+itself.
+While the command gets elaborated, the `InfoTree` gets constructed, storing important information,
+that is then also used during elaboration.
+
+Once Lean finishes elaborating a command, it also creates the `InfoTree`s.
+And we can inspect them!
+-/
+
+inspectIT #check Nat.succ
+inspectIT #check nnn.succ
+/-!
+## The takeaway
+
+When inspecting commands,
+* we get superficial information by analysing the `Syntax`;
+* we get in-depth (and virtually complete) information, by analysing the `InfoTree`s.
+
+## What should I expect to find in an `InfoTree`
+
+As you move the cursor through a command, the `InfoView` displays changing information.
+More or less, every time that the display changes, corresponds to a new node in an `InfoTree`.
+Some of the information stored in the `Info` input to that node is then displayed in the
+`InfoView`.
+
+Depending on the `Info` at each node, different kinds of "context" are accessible.
+
+### `TacticInfo` nodes in an `InfoTree`
+For instance at a `TacticInfo` node, we can find information about
+* which `Syntax` node is responsible for this `Info`;
+* which elaborator handled the corresponding `Syntax`;
+* the metavariable contexts before and after the tactic is evaluated;
+* the list of goals before and after the tactic is evaluated.
+
+In turn, the metavariable contexts contain information about the types of each goals,
+their universe levels, which relationships there are between all the metavariables that have been
+created so far, local contexts and so on.
+
+### `ContextInfo` and `InfoTree`s
+A `ContextInfo` is not actually stored anywhere in an `InfoTree`
+(although the related `PartialContextInfo` is).
+Even so, a `ContextInfo` is similar to information that could be included in an `InfoTree`.
+The main difference is that the data contained in `InfoTree`s tends to be "local": it is a snapshot
+of what is available at a particular moment.
+A `ContextInfo` is more "global": it links the information that was available *before* the command
+was elaborated to the specific modifications that happened in the course of elaboration.
+
+For instance, the `Environment` and the whole content of the file is contained in a `ContextInfo`.
+Also, which `option`s are set, what is the current `namespace` and what namespaces are `open`
+is all stored in a `ContextInfo`.
+
+Note that this information can be modified within a single command, since there can be nested
+`set_option ... in ...`, `variable ... in ...` and similar modifying any given command.
+
+In summary, a `ContextInfo` really does provide a "context" for the data stored in the `Info` nodes:
+the `ContextInfo` is essential to "read" correctly the information of the `Info` nodes.
+
+# The `tacticAnalysis` framework
+
+Finally, we made it here!
+
+
+-/
+
+inspectIT /-! -/ --example : True := trivial
+
+end Talk
